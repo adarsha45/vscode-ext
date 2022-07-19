e5cfd9b78f4ae5762430aadd3379089845a5f0f1
adarsha45 <shresthaadarsha957@gmail.com>
2022-07-19 11:49:12 +0545

    check

--- src/newfun.ts
+++ src/newfun.ts
@@ -1,8 +1,12 @@
 import { match } from 'assert';
+import { type } from 'os';
 import { toNamespacedPath } from 'path';
 import * as vscode from 'vscode';
-import * as jsonss from "../json/ada.json";
+import * as jsonss from '../json/ada.json' assert {type:"json"};
+import * as credentials from '../json/jwt.json' assert {type:"json"};
+
 let methods = jsonss.athenticatemethod;
+let apidatas = credentials.credentials;
 export async function newfun(){
     // console.log(methods);
     let authenticatemethod = methods.map( json => {
@@ -12,20 +16,26 @@ export async function newfun(){
         }
     }
         );
+<<<<<<< Updated upstream
         // console.log(authenticatemethod);
     
     
+=======
+        console.log(authenticatemethod);
+
+>>>>>>> Stashed changes
     const lmethod:string | any = await vscode.window.showQuickPick(
         authenticatemethod,{
             matchOnDetail:true
       }
       );
-
+      let CODE:any;
       if(lmethod.label === 'JWT authenticate'){
         vscode.window.showWarningMessage('JWT Authenticatication Enabled');
-        const CODE:any = await vscode.window.showInputBox({
+        CODE = await vscode.window.showInputBox({
             placeHolder: 'Plese!! Enter Your Email',
                      });
+<<<<<<< Updated upstream
         //   console.log(CODE);
                    
     }
@@ -33,3 +43,13 @@ export async function newfun(){
  }
 
  console.log(jsonss.squadName);
+=======
+                    }
+
+                    apidatas.forEach(apid => {apid.SF_CONSUMER_KEY = CODE;console.log(apid.SF_CONSUMER_KEY);});
+
+
+                  
+
+                }
+>>>>>>> Stashed changes
