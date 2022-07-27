c888dd025f526fd99f9aefebaee62028413c972c
adarsha45 <shresthaadarsha957@gmail.com>
2022-07-27 12:09:18 +0545

    ff

--- src/newfun.ts
+++ src/newfun.ts
@@ -1,31 +1,111 @@
-import { match } from 'assert';
-import { toNamespacedPath } from 'path';
 import * as vscode from 'vscode';
-import * as jsonss from "../json/ada.json";
-let methods = jsonss.athenticatemethod;
-export async function newfun(){
-    console.log(methods);
-    let authenticatemethod = methods.map( json => {
-        return {
-            label:json.name,
-            description:json.label,
-        }
-    }
-        );
-        console.log(authenticatemethod);
-    
-    
-    const lmethod:string | any = await vscode.window.showQuickPick(
-        authenticatemethod,{
-            matchOnDetail:true
-      }
-      );
+import * as cp from "child_process";
+import { isAnyArrayBuffer } from 'util/types';
+const fs = require('fs');
+let parsedJsonData;
+var jenkins = require('jenkins')({ baseUrl: 'http://admin:115ee90296f171497b5ee55b81db2ad4a6@localhost:8080', crumbIssuer: true });
 
-      if(lmethod.label === 'JWT authenticate'){
-        vscode.window.showWarningMessage('JWT Authenticatication Enabled');
-        const CODE:any = await vscode.window.showInputBox({
-            placeHolder: 'Plese!! Enter Your Email',
-                     });
-          console.log(CODE);
-                    }
-                }
+export async function newfun(){
+//     let username:any;
+jenkins.info(function(err:any, data:any) {
+  if (err) {throw err;}
+ 
+  console.log('info', data);
+});
+
+jenkins.job.build({ name: 'antpractice' , token: "xyz123"}, function(err:any) {
+  if (err) {
+    vscode.window.showErrorMessage("Oh no");
+  }
+  vscode.window.showInformationMessage("Build triggerd");
+});
+//     let consumerKey:any;
+//     let serverKeyFile:any;
+//     let setalias:any;
+
+//     const beforedata = fs.readFileSync('/home/adarsha/Documents/extension/firstextension/first-ext/json/jwt.json', 'utf8');
+//     const jsonData = JSON.parse(beforedata);
+//     console.log(jsonData);
+
+//     username = await vscode.window.showInputBox({
+//         placeHolder: 'Plese!! Enter Your username',
+//                  });
+//       console.log(username);
+//       consumerKey = await vscode.window.showInputBox({
+//         placeHolder: 'Plese!! Enter Your consumer key?',
+//                  });
+//       console.log(consumerKey);
+//       serverKeyFile = await vscode.window.showInputBox({
+//         placeHolder: 'Plese!! Enter Your serverkey?',
+//                  });
+//       console.log(serverKeyFile);
+//       setalias= await vscode.window.showInputBox({
+//         placeHolder: 'Plese!!setalias',
+//                  });
+//       console.log(setalias);
+//       let items:any = [{
+//         label:"loin.salesforce.com",
+//         description:"https://login.salesforce.com",
+//       },
+//       {
+//         label:"test.salesforce.com",
+//         description:"https://test.salesforce.com",
+//       }];
+
+//       const instanceurl:string | any = await vscode.window.showQuickPick(
+//         items,{
+//             matchOnDetail:true,            
+//       }
+//       );
+//       console.log(instanceurl);
+//     const newData = {
+//         sfUsername: username,
+//         sfConsumer:consumerKey,
+//         sfServerkey:serverKeyFile,
+//         sfInstanceurl:instanceurl,
+//         sfsetalias:setalias
+//     } ;
+//     const stringify = JSON.stringify(newData);
+//     await fs.writeFile('/home/adarsha/Documents/extension/firstextension/first-ext/json/jwt.json', stringify, err => {
+//         // error checking
+//         if(err) {throw err;};
+        
+//         console.log("New data added");
+//     });
+   
+//     const {exec} = require("child_process");
+//     const execShell = function(cmd: string){
+//         exec(cmd, (error:any , stdout:any, stderr:any) => {
+//             console.log("stdout: " + stdout);
+
+//             console.log("stderr: " + stderr);
+
+//             if (error !== null){
+
+//                 console.log("exec error: " + error);
+//             }
+
+    
+// }
+//     );};
+//     let jsonafterData:any;
+//     let usernamefromjson:any;
+//     let consumerkeyfromjson:any;
+//     let skfromjson:any;
+//     let aliasfromjson:any;
+//     let url:any;
+//     let ada:any;
+   
+//     setTimeout(function(){
+//         const afterdata =  fs.readFileSync('/home/adarsha/Documents/extension/firstextension/first-ext/json/jwt.json', 'utf8');
+//          jsonafterData = JSON.parse(afterdata);
+//           usernamefromjson = jsonafterData.sfUsername;
+//           consumerkeyfromjson = jsonafterData.sfConsumer;
+//           skfromjson = jsonData.sfServerkey;
+//           aliasfromjson = jsonafterData.sfsetalias;
+//           url = jsonafterData.sfInstanceurl.description;
+//        let b = execShell(`sfdx force:auth:jwt:grant --instanceurl ${url} --clientid ${consumerkeyfromjson} --username ${usernamefromjson} --jwtkeyfile ${skfromjson} --setdefaultdevhubusername --setalias ${aliasfromjson}`);
+//     }, 1000);
+//      // let a = await execShell('sfdx auth:web:login ');
+}
+ 
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,10 +1,11 @@
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
 
+var glob = require("glob");
+
+var mochA = require("mocha");
 export function run(): Promise<void> {
 	// Create the mocha test
-	const mocha = new Mocha({
+	const mocha = new mochA({
 		ui: 'tdd',
 		color: true
 	});
@@ -12,17 +13,17 @@ export function run(): Promise<void> {
 	const testsRoot = path.resolve(__dirname, '..');
 
 	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err:any, files:any) => {
 			if (err) {
 				return e(err);
 			}
 
 			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+			files.forEach((f:any) => mocha.addFile(path.resolve(testsRoot, f)));
 
 			try {
 				// Run the mocha test
-				mocha.run(failures => {
+				mocha.run((failures:any) => {
 					if (failures > 0) {
 						e(new Error(`${failures} tests failed.`));
 					} else {
